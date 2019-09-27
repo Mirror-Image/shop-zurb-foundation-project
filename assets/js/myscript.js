2ffdb9dead88dd6b536cebf33191ec75c09232eb
MaksimBorovik <borovik.maksim@gmail.com>
2019-09-27 15:59:43 +0300

    added elevatezoom pluging to the product-page. Fixed minor bugs

---
+++ assets/js/myscript.js
@@ -0,0 +1,32 @@
+$('#zoom_01').elevateZoom({
+  easing : true,
+  zoomWindowWidth:800,
+  zoomWindowHeight:800, zoomWindowFadeIn: 500,
+  zoomWindowFadeOut: 500,
+  lensFadeIn: 500,
+  lensFadeOut: 500
+});
+$('#zoom_02').elevateZoom({
+  easing : true,
+  zoomWindowWidth:800,
+  zoomWindowHeight:800, zoomWindowFadeIn: 500,
+  zoomWindowFadeOut: 500,
+  lensFadeIn: 500,
+  lensFadeOut: 500
+});
+$('#zoom_03').elevateZoom({
+  easing : true,
+  zoomWindowWidth:800,
+  zoomWindowHeight:800, zoomWindowFadeIn: 500,
+  zoomWindowFadeOut: 500,
+  lensFadeIn: 500,
+  lensFadeOut: 500
+});
+$('#zoom_04').elevateZoom({
+  easing : true,
+  zoomWindowWidth:800,
+  zoomWindowHeight:800, zoomWindowFadeIn: 500,
+  zoomWindowFadeOut: 500,
+  lensFadeIn: 500,
+  lensFadeOut: 500
+});
