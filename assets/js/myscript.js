46cc7efd96d0a3ac4af6438c18bf6ea50ef9d466
MaksimBorovik <borovik.maksim@gmail.com>
2019-09-28 00:51:26 +0300

    added a sessionStorage functionality for cart

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,71 +1,97 @@
 let cartCounterDisplay = document.querySelector('#cart-counter');
+let cartCalloutDisplay = document.querySelector('#cart-callout');
 let buttonsContainer = document.querySelector('.content__bestsellers');
-// let currentPrice = document.querySelector('.current-price');
 
-let cartCounter = 0;
-let cartPrice = 0;
+let amount = +getGlobalVar('amount');
+let quantity = +getGlobalVar('quantity');
+let callout = +getGlobalVar('callout');
 
-let btnClickHandler = (e) => {
-  let target = e.target;
+let cartCounter = quantity;
+let cartPrice = amount;
 
-  if (target.classList.contains('item-action-cart')) {
-    // cartCounterDisplay.innerHTML += cartCounter;
+cartCounterDisplay.innerHTML = `&nbsp;${cartCounter} item(s) -&nbsp; ${cartPrice} $`;
 
-    buttonsContainer.removeEventListener('click', btnClickHandler);
+function saveGlobalVar(name, value) {
+  sessionStorage.setItem(name, value); // сохраняем в localStorage значение
+}
+function getGlobalVar(name) {
+  return sessionStorage.getItem(name); // получаем значение свойства localStorage
+}
 
-    const currentPrice = target.parentElement.previousElementSibling
-      .querySelector('.current-price').textContent.replace(/\D/g, '');
+buttonsContainer.onclick = function(event) {
+  let button = event.target.closest('.item-action-cart');
+  if(!button) return;
+  if(!buttonsContainer.contains(button)) return;
 
-    // console.log(+currentPrice);
-
-    cartCounterDisplay.innerHTML = (() => {
-      cartPrice += +currentPrice;
-      cartCounter = ++cartCounter;
-      return `&nbsp;${cartCounter} item(s) -&nbsp; ${cartPrice} $`;
-      // console.log(currentPrice);
-      // console.log(typeof cartPrice);
-    })();
-
-    const restoreHTML = target.innerHTML;
-
-    setTimeout(() => {
-      target.innerHTML = restoreHTML;
-      buttonsContainer.addEventListener('click', btnClickHandler);
-    }, 1000);
-  }
+  btnClickHandler(button);
 };
 
-buttonsContainer.addEventListener('click', btnClickHandler);
+let btnClickHandler = (e) => {
+  const currentPrice = (e.parentElement.previousElementSibling
+  .querySelector('.current-price') ||
+  e.parentElement.parentElement.previousElementSibling
+  .querySelector('.current-price')).textContent.replace(/\D/g, '');
+  // console.log(currentPrice);
 
-// $('#zoom_01').elevateZoom({
-//   easing : true,
-//   zoomWindowWidth:800,
-//   zoomWindowHeight:800, zoomWindowFadeIn: 500,
-//   zoomWindowFadeOut: 500,
-//   lensFadeIn: 500,
-//   lensFadeOut: 500
-// });
-// $('#zoom_02').elevateZoom({
-//   easing : true,
-//   zoomWindowWidth:800,
-//   zoomWindowHeight:800, zoomWindowFadeIn: 500,
-//   zoomWindowFadeOut: 500,
-//   lensFadeIn: 500,
-//   lensFadeOut: 500
-// });
-// $('#zoom_03').elevateZoom({
-//   easing : true,
-//   zoomWindowWidth:800,
-//   zoomWindowHeight:800, zoomWindowFadeIn: 500,
-//   zoomWindowFadeOut: 500,
-//   lensFadeIn: 500,
-//   lensFadeOut: 500
-// });
-// $('#zoom_04').elevateZoom({
-//   easing : true,
-//   zoomWindowWidth:800,
-//   zoomWindowHeight:800, zoomWindowFadeIn: 500,
-//   zoomWindowFadeOut: 500,
-//   lensFadeIn: 500,
-//   lensFadeOut: 500
-// });
+  cartCalloutDisplay.innerHTML = "Your cart isn't empty! Check it out!";
+  saveGlobalVar('callout', cartCalloutDisplay.innerHTML);
+
+  cartCounterDisplay.innerHTML = (() => {
+    cartPrice += +currentPrice;
+    cartCounter = ++cartCounter;
+
+    saveGlobalVar('amount', cartPrice);
+    saveGlobalVar('quantity', cartCounter);
+
+    return `&nbsp;${cartCounter} item(s) -&nbsp; ${cartPrice} $`;
+  })();
+
+
+
+  console.log(amount);
+  console.log(quantity);
+  console.log(callout);
+
+  // const restoreHTML = target.innerHTML;
+  //
+  // setTimeout(() => {
+  //   target.innerHTML = restoreHTML;
+  //   // buttonsContainer.addEventListener('click', btnClickHandler);
+  // }, 1000);
+};
+
+
+
+
+$('#zoom_01').elevateZoom({
+  easing : true,
+  zoomWindowWidth:800,
+  zoomWindowHeight:800, zoomWindowFadeIn: 500,
+  zoomWindowFadeOut: 500,
+  lensFadeIn: 500,
+  lensFadeOut: 500
+});
+$('#zoom_02').elevateZoom({
+  easing : true,
+  zoomWindowWidth:800,
+  zoomWindowHeight:800, zoomWindowFadeIn: 500,
+  zoomWindowFadeOut: 500,
+  lensFadeIn: 500,
+  lensFadeOut: 500
+});
+$('#zoom_03').elevateZoom({
+  easing : true,
+  zoomWindowWidth:800,
+  zoomWindowHeight:800, zoomWindowFadeIn: 500,
+  zoomWindowFadeOut: 500,
+  lensFadeIn: 500,
+  lensFadeOut: 500
+});
+$('#zoom_04').elevateZoom({
+  easing : true,
+  zoomWindowWidth:800,
+  zoomWindowHeight:800, zoomWindowFadeIn: 500,
+  zoomWindowFadeOut: 500,
+  lensFadeIn: 500,
+  lensFadeOut: 500
+});
