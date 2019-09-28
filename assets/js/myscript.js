f99c19a79e7351a6b9541703750bdc17e3fc4b76
MaksimBorovik <borovik.maksim@gmail.com>
2019-09-28 03:27:56 +0300

    fixed a size of the elevateZoom window from 800x800 to 400x400px

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -86,32 +86,36 @@ let btnClickHandler = (e) => {
 
 $('#zoom_01').elevateZoom({
   easing : true,
-  zoomWindowWidth:800,
-  zoomWindowHeight:800, zoomWindowFadeIn: 500,
+  zoomWindowWidth: 400,
+  zoomWindowHeight: 400,
+  zoomWindowFadeIn: 500,
   zoomWindowFadeOut: 500,
   lensFadeIn: 500,
   lensFadeOut: 500
 });
 $('#zoom_02').elevateZoom({
   easing : true,
-  zoomWindowWidth:800,
-  zoomWindowHeight:800, zoomWindowFadeIn: 500,
+  zoomWindowWidth: 400,
+  zoomWindowHeight: 400,
+  zoomWindowFadeIn: 500,
   zoomWindowFadeOut: 500,
   lensFadeIn: 500,
   lensFadeOut: 500
 });
 $('#zoom_03').elevateZoom({
   easing : true,
-  zoomWindowWidth:800,
-  zoomWindowHeight:800, zoomWindowFadeIn: 500,
+  zoomWindowWidth: 400,
+  zoomWindowHeight: 400,
+  zoomWindowFadeIn: 500,
   zoomWindowFadeOut: 500,
   lensFadeIn: 500,
   lensFadeOut: 500
 });
 $('#zoom_04').elevateZoom({
   easing : true,
-  zoomWindowWidth:800,
-  zoomWindowHeight:800, zoomWindowFadeIn: 500,
+  zoomWindowWidth: 400,
+  zoomWindowHeight: 400,
+  zoomWindowFadeIn: 500,
   zoomWindowFadeOut: 500,
   lensFadeIn: 500,
   lensFadeOut: 500
